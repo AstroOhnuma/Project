65847efab2d55749aa97d3f51baa9ebfea3c6d41
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-01 13:59:46 -0400

    Updated from Brython Server: 11/1/2017 1:59:45 PM

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,20 @@
+#Astro Ohnuma
+#11/1/17
+#tictactoe.py - first project, tictactoe without graphics
 
+square1 = 1
+square2 = 2
+square3 = 3
+square4 = 4
+square5 = 5
+square6 = 6
+square7 = 7
+square8 = 8
+square9 = 9
+
+def printboard():
+    for i in range(1):
+        print('__ '*3)
+    for i in range(1):
+        print('|  '*3)
+printboard()
