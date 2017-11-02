6a07acba5ee1ece131c606cf7e16d6d4f0759fd3
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-02 14:43:11 -0400

    Updated from Brython Server: 11/2/2017 2:43:10 PM

--- tictactoe.py
+++ tictactoe.py
@@ -33,5 +33,7 @@ def winner():
         return False
 def fullboard():
     if sqaure1 == x or square1 == o and sqaure2 == x or square2 == o and sqaure3 == x or square3 == o and sqaure4 == x or square4 == o and sqaure5 == x or square5 == o and sqaure6 == x or square6 == o and sqaure7 == x or square7 == o and sqaure8 == x or square8 == o and sqaure9 == x or square9 == o:
-        
+        return True
+    else:
+        return False
 printboard()
