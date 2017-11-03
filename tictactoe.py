cb71a680700421da432fd7d09de2449c714451a0
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-11-03 14:44:59 -0400

    Updated from Brython Server: 11/3/2017 2:44:57 PM

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,10 @@
 #Astro Ohnuma
 #11/1/17
 #tictactoe.py - first project, tictactoe without graphics
-
+from random import randint
+win = 0
+player = 'none'
+com = 'also none'
 square1 = 1
 square2 = 2
 square3 = 3
@@ -32,12 +35,54 @@ def winner():
     else:
         return False
 def fullboard():
-    if sqaure1 == x or square1 == o and sqaure2 == x or square2 == o and sqaure3 == x or square3 == o and sqaure4 == x or square4 == o and sqaure5 == x or square5 == o and sqaure6 == x or square6 == o and sqaure7 == x or square7 == o and sqaure8 == x or square8 == o and sqaure9 == x or square9 == o:
+    if sqaure1 == 'X' or square1 == 'O' and sqaure2 == 'X' or square2 == 'O' and sqaure3 == 'X' or square3 == 'O' and sqaure4 == 'X' or square4 == 'O' and sqaure5 == 'X' or square5 == 'O' and sqaure6 == 'X' or square6 == 'O' and sqaure7 == 'X' or square7 == 'O' and sqaure8 == 'X' or square8 == 'O' and sqaure9 == 'X' or square9 == 'O':
         return True
     else:
         return False
 if __name__ == '__main__':
+    '''
+    first = randint(1,2)
+    if first == 1:
+        print('Player goes first!')
+    else:
+        print('Computer goes first!')
     choice = input('Would you like to be Xs or Os?')
-    if choice == 'x' or 'X':
+    if choice == 'x' or choice == 'X':
+        player = x
+        com = o
+    elif choice == 'o' or choice == 'O':
+        player = o
+        com = x'''
+    printboard()
+    while win < 1:
+        turn = int(input('Where would you like to go?: '))
+        if turn == 1:
+            square1 = 'X'
+            printboard()
+        elif turn == 2:
+            square2 = 'X'
+            printboard()
+        elif turn == 3:
+            square3 = 'X'
+            printboard()
+        elif turn == 4:
+            square4 = 'X'
+            printboard()
+        elif turn == 5:
+            square5 = 'X'
+            printboard()
+        elif turn == 6:
+            square6 = 'X'
+            printboard()
+        elif turn == 7:
+            square7 = 'X'
+            printboard()
+        elif turn == 8:
+            square8 = 'X'
+            printboard()
+        elif turn == 9:
+            square9 = 'X'
+            printboard()
         
-printboard()
+        
+    
