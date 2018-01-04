5ef9989e7302a3974c5e293ca937b5b19071f8dc
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2018-01-04 11:06:59 -0500

    Updated from Brython Server: 1/4/2018 11:06:58 AM

--- conwaysgameoflife.py
+++ conwaysgameoflife.py
@@ -9,6 +9,7 @@ def buildboard():
     for row in range(0,10):
         for col in range(0,10):
             print(board[row][col],' ',end = '')
+        print()
 def redrawall():
     for item in App().spritelist[:]:
         item.destroy()
