7de1275d5814712a480f78b28e88bafe13ea378e
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2018-01-07 16:16:59 -0500

    Updated from Brython Server: 1/7/2018 4:16:57 PM

--- conwaysgameoflife.py
+++ conwaysgameoflife.py
@@ -2,9 +2,10 @@
 #12/14/17
 #conwaysgameoflife.py - Mathematical model of life and population growth
 from ggame import *
-
+#builds the matrix for the game board
 def buildboard():
     return data['board']
+#deletes all previous graphics, crreates and sprites the board and checks if a cell is living or dead, spriting it accordingly
 def redrawall():
     for item in App().spritelist[:]:
         item.destroy()
@@ -28,6 +29,7 @@ def redrawall():
                 Sprite(livingcell, (col*30,row*30))
             else:
                 Sprite(deadcell, (col*30,row*30))
+#checks how many neighbors a cell has
 def numneighbors(num1,num2):
     count = 0
     if num1 > 0 and num2 > 0 and data['board'][num1-1][num2-1] == 1:
@@ -47,6 +49,7 @@ def numneighbors(num1,num2):
     if num2 > 0 and data['board'][num1][num2-1] == 1:
         count += 1
     return count
+#creates a new matrix for the board, checking the rules of the game of life and spriting dead cells and live cells accordingly, and then sets the old matrix equal to the new matrix
 def nextgeneration():
     newboard = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
     for row in range(0,10):
@@ -62,6 +65,7 @@ def nextgeneration():
                 newboard[row][col] = 1
     data['board'] = newboard
     redrawall()
+#makes it so where the mouse is clicked, that cell there becomes a 1 and will become a live cell and detects wether the mouse clicks the next generation button and runs next generation.
 def mouseclick(event):
         if event.y//30 > 9 or event.x//30 > 9:
             nextgeneration()
@@ -69,6 +73,7 @@ def mouseclick(event):
             data['board'][event.y//30][event.x//30] = 1
             print('You clicked row',int(event.y/30),'and column',int(event.x/30))
             redrawall()
+#runs the game
 if __name__ == '__main__':
     data = {}
     data['board'] = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
